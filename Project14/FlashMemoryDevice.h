e253648367c5cbfa112f489d98413abe82c146f1
hyesu <phs9575@gmail.com>
2023-06-05 15:48:06 +0900

    add feature/read

--- Project14/FlashMemoryDevice.h
+++ Project14/FlashMemoryDevice.h
@@ -1,11 +1,18 @@
 #pragma once
+#include <combaseapi.h>
 
 /*
  * This class represents the interface to a Flash Memory Device. The hardware has only two methods - 'read' and 'write'
  * However, the interface for using the device is a lot more complex than that. It is outlined in the top-level README file.
  */
 
-class FlashMemoryDevice
+
+interface FMDAPI {
+	virtual unsigned char read(long address) = 0;
+	virtual void write(long address, unsigned char data) = 0;
+};
+
+class FlashMemoryDevice : public FMDAPI
 {
 public:
     virtual unsigned char read(long address) = 0;
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -2,7 +2,36 @@
 #include <gmock/gmock.h>
 #include "../Project14/DeviceDriver.cpp"
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+using namespace testing;
+using namespace std;
+
+
+class FMDMock:public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD((unsigned char), read, (long addr), (override));
+	MOCK_METHOD(void, write, (long addr, unsigned char data), (override));
+};
+
+TEST(TestCaseName, FiveRead) {
+	FMDMock fmd_mock;
+	DeviceDriver driver(&fmd_mock);
+
+	EXPECT_CALL(fmd_mock, read)
+		.Times(5);
+
+	driver.read(0xA);
+}
+TEST(TestCaseName, ReadException) {
+	FMDMock fmd_mock;
+	DeviceDriver driver(&fmd_mock);
+
+	EXPECT_CALL(fmd_mock, read(0x2))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x7))
+		.WillOnce(Return(0x3));
+
+	EXPECT_THROW(driver.read(0x2), myException);
 }
